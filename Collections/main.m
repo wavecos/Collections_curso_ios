0c3d4db122b6d8a7248eeb633ce7096f014062e9
Alfredo Arias <alfre.arias@gmail.com>
2013-09-28 09:34:51 -0400

    Initial Commit

---
+++ Collections/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Collections
+//
+//  Created by Tekhne on 9/28/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
